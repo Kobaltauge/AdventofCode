c7b63a78efe68a7c2c86cd6d8b5877090612b5a1
Castell, Massimo <Massimo.Castell@durr.com>
2020-12-07 17:14:22 +0100

    day07

--- 2020/day07.py
+++ 2020/day07.py
@@ -6,14 +6,54 @@ with open("day_07.txt") as f:
 with open("day_07_exampl.txt") as f:
     baglistraw = list(f.read().splitlines())
 
+def check_white_yellow(bag, bagcont, count):
+    # print(bag,bagcont)
+    if "bright white" in bag:
+        # print(bag,bagcont)
+        return True
+    elif "muted yellow" in bag:
+        # print(bag,bagcont)
+        return True
+    elif ("dark orange" in bag) and (any("bright white" in c for c in bagcont) or any("muted yellow" in c for c in bagcont)):
+        # print(bag,bagcont)
+        return True
+    elif ("light red" in bag) and (any("bright white" in c for c in bagcont) or any("muted yellow" in c for c in bagcont)):
+        # print(bag,bagcont)
+        return True
+    else:
+        for bagitemraw in bagcont:
+            bagitem = bagitemraw.split()[1] + " " + bagitemraw.split()[2]
+            if not bagitem == "other bags":
+                if bagitem in bags:
+                    if check_white_yellow(bagitem, bags[bagitem], count):
+                        return True
+            else:
+                return False
+        return False
 
-baglist = {}
+    # if any("bright white" in c for c in bagcont):
+    #     count +=1
+    #     return count
+    # elif bag in bags:
+    #     check_white_yellow(bag, bagcont, count)
+    #     return count
 
-# (\w+ \w+) bags contain (((\d+)|(\bno\b)) (\w+ \w+) (bags|bag)[,|.]\s*)*$
+bags = {}
 
 for bag in baglistraw:
-    bagreg = re.match(r"(\w+ \w+) bags contain (([\d+]) (\w+ \w+) (bags|bag)[,|.]\s*)*$", bag)
-    print(bagreg[0])
-    print(bagreg[1]) 
+    bagsplit = bag.split("bags contain")
+    bagcolor = bagsplit[0].strip()
+    bagcont = bagsplit[1].replace(".", "").split(",")
+    bags[bagcolor] = bagcont
 
-    print("ende")
+count = 0
+gesamt = 0
+valid = set()
+for bag in bags:
+    bagcont = bags[bag]
+    if check_white_yellow(bag, bagcont, count):
+        valid.add(bag)
+    gesamt += 1
+    print(gesamt)
+
+print(len(valid))
