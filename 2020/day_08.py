abd420ba050663c62580e562128d103a2872a552
Massimo <massimo.castell@gmail.com>
2020-12-09 23:33:19 +0100

    day 8

--- 2020/day_08.py
+++ 2020/day_08.py
@@ -26,7 +26,6 @@ while True:
     elif op == "nop":
         index += 1
 
-print(stack)
 print(accumulator)
 print("part 2")
 
@@ -34,29 +33,46 @@ with open("day_08_new.txt") as f:
     oplist = f.read().splitlines()
 
 
-accumulator = 0
-accumulator_old = 0
-index = 0
-stack = []
+def check(oplist):
+    accumulator = 0
+    accumulator_old = 0
+    index = 0
+    stack = []
 
-while True:
-    if index == 611:
-        print("end")
-        break
-    opraw = oplist[index]
+    while True:
+        if index == 611:
+            print("end")
+            return accumulator
+        opraw = oplist[index]
+        op = opraw[:3]
+        data = opraw[4:]
+        # print(index, op, int(data), accumulator_old, accumulator)
+        stack.append(index)
+        if op == "acc":
+            accumulator_old = int(accumulator)
+            accumulator += int(data)
+            index += 1
+        elif op == "jmp":
+            index += int(data)
+            if index in stack:
+                return False
+        elif op == "nop":
+            index += 1
+
+for i in range(0,len(oplist)):
+    oplistmod = oplist.copy()
+    opraw = oplist[i]
     op = opraw[:3]
     data = opraw[4:]
-    # print(index, op, int(data), accumulator_old, accumulator)
-    stack.append(index)
-    if op == "acc":
-        accumulator_old = int(accumulator)
-        accumulator += int(data)
-        index += 1
-    elif op == "jmp":
-        index += int(data)
-        if index in stack:
-            break
+    if op == "jmp":
+        oplistmod[i] = str("nop "+data)
+        solution = check(oplistmod)
+        if solution:
+            print(solution)
     elif op == "nop":
-        index += 1
-print(index)
-print(accumulator)
+        oplistmod[i] = str("jmp "+data)
+        solution = check(oplistmod)
+        if solution:
+            print(solution)
+        
+    
