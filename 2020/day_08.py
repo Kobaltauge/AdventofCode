e3882f8e6ee80fcfe42c6641aad6d7fed1790a3f
massimo <Massimo.Castell@gmail.com>
2020-12-09 07:53:16 +0100

    day 08

--- 2020/day_08.py
+++ 2020/day_08.py
@@ -3,28 +3,31 @@ import time
 with open("day_08.txt") as f:
     oplist = f.read().splitlines()
 
-with open("day_08_exampl.txt") as f:
-    oplist = f.read().splitlines()
+#with open("day_08_exampl.txt") as f:
+    #oplist = f.read().splitlines()
 
 accumulator = 0
+accumulator_old = 0
 index = 0
+stack = []
 
 while True:
     opraw = oplist[index]
     op = opraw[:3]
     data = opraw[4:]
+    print(index, op, int(data), accumulator_old, accumulator)
+    stack.append(index)
     if op == "acc":
         accumulator_old = int(accumulator)
         accumulator += int(data)
-        print(index, int(data), accumulator_old, accumulator)
-        time.sleep(0.1)
-        # if accumulator < 5:
-        #     break
+        time.sleep(0.2)
         index += 1
     elif op == "jmp":
         index += int(data)
+        if index in stack:
+            break
     elif op == "nop":
         index += 1
 
-print(accumulator_old)
+print(accumulator)
 
