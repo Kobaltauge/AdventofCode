c7eac0b0900495da4be518e6d213c2a576de55ad
Castell, Massimo <Massimo.Castell@durr.com>
2020-12-14 17:05:52 +0100

    day 14

--- 2020/day_14.py
+++ 2020/day_14.py
@@ -5,16 +5,50 @@ with open("day_14.txt") as f:
 instructions = [i.replace('\n','').split(" = ") for i in instructions] 
 
 memory = dict()
+actmask = ""
 
 for instr in instructions:
-    actmask = ""
     if instr[0] == "mask":
         actmask = instr[1]
     if instr[0][:3] == "mem":
-        newmem = instr[0][4:-2]
-        if newmem in memory.keys():
-            pass
-        else:
-            # memory[newmem] = instr[1]
-            memory[newmem] = str(f"{int(instr[1]):036b}")
+        memadr = instr[0][4:-1]
+        newmemdec = instr[1]
+        newmembin = list(str(f"{int(instr[1]):036b}"))
+        masklist = list(actmask)
+        for idx in range(len(newmembin)):
+            if masklist[idx] == "X":
+                pass
+            elif masklist[idx] == "1":
+                newmembin[idx] = "1"
+            elif masklist[idx] == "0":
+                newmembin[idx] = "0"
+        newmembin = "".join(newmembin)
+        memory[memadr] = (int(newmembin, 2), newmembin)
+
+print(sum([int(d[0]) for d in memory.values()]))
 print("part 2")
+
+for instr in instructions:
+    if instr[0] == "mask":
+        actmask = instr[1]
+    if instr[0][:3] == "mem":
+        memadrorg = instr[0][4:-1]
+        memadr = []
+        for count in range(2**actmask.count('X')):
+            binstring = list(str("{0:b}".format(count)).zfill(actmask.count('X')))
+            pass
+        newmemdec = instr[1]
+        newmembin = list(str(f"{int(instr[1]):036b}"))
+        masklist = list(actmask)
+        for idx in range(len(newmembin)):
+            if masklist[idx] == "X":
+                pass
+            elif masklist[idx] == "1":
+                newmembin[idx] = "1"
+            elif masklist[idx] == "0":
+                newmembin[idx] = "0"
+        newmembin = "".join(newmembin)
+        for m in memadr:
+            memory[m] = (int(newmembin, 2), newmembin)
+
+print(sum([int(d[0]) for d in memory.values()]))
