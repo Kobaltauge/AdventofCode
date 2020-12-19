78ed078af39f00f66ada864150ad0f306e3c7f62
Castell, Massimo <Massimo.Castell@durr.com>
2020-12-19 19:46:33 +0100

    day 17

--- 2020/day_19.py
+++ 2020/day_19.py
@@ -9,11 +9,22 @@ for rule in rules:
     ruleno, ruletext = rule.split(":")[0], rule.split(":")[1].strip()
     rulesdict[str(ruleno)] = ruletext
 
-def check(checkstring, idx):
-    if checkstring[idx] == "a" or checkstring[idx] == "b":
+def check(checkstring, idx, rule, erg):
+    text = rulesdict[str(rule)]
+    if checkstring[idx] == text.replace("\"",""):
         return True
     else:
-        return check(checkstring, idx+1)
+        if len(text.split()) == 1:
+            return erg.append(check(checkstring, idx+1, str(text).strip()))
+        elif len(text) == 2:
+            text1, text2 = checkstring[idx].split()
+            return erg.append(all((check(checkstring, idx+1, str(text1).strip()) and checkstring(checkstring, idx+2), str(text2).strip())))
+        else:
+            text1, text2 = text.split("|")
+            elem1, elem2 = text1.split()
+            elem3, elem4 = text2.split()
+            return erg.append(all((check(checkstring, idx+1, str(elem1).strip()) and checkstring(checkstring, idx+2), str(elem2).strip()), \
+                    (check(checkstring, idx+1, str(elem3).strip()) and checkstring(checkstring, idx+2), str(elem4).strip())))
 
 # def check(checkstring, idx, check1, check2):
 #     if idx > len(checkstring):
@@ -30,7 +41,7 @@ def check(checkstring, idx):
 
 counter = 0
 for code in codes:
-    if all(check(code, 0)):
+    if all(check(code, 0, 8, []), code(code, 0, 11, [])):
         counter += 1
 
 print("part 2")
