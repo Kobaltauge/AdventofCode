84006a829894f10c3217707a1d4d45c35a8c7490
MassimoCastell <massimo.castell@gmail.com>
2021-12-13 00:36:39 +0100

    save

--- python_2021/day09/day_09.py
+++ python_2021/day09/day_09.py
@@ -1,5 +1,5 @@
-#import math
 import re
+import numpy as np
 
 with open("day_09.txt") as f:
     input = f.read()
@@ -33,6 +33,43 @@ for y in range(0, dimy):
 
 print(f"result part 1: {sum([i[1]+1 for i in minis])}")
 
-map = [re.sub('[0-8]', '0', i) for i in map]
+# map = [re.sub('[0-8]', '0', i) for i in map]
+
+def get_adjacent_cells(grid, cell):
+    adjdict = {'n':(0,-1),'ne':(1,-1),'e':(1,0),'se':(1,1),'s':(0,1),'sw':(-1,1),'w':(-1,0),'nw':(-1,-1)}
+    cells = []
+
+    if cell[0] >= len(grid[0])-1:
+        adjdict = {k:v for k,v in adjdict.items() if not 'e' in k}
+    if cell[0] <= 0:
+        adjdict = {k:v for k,v in adjdict.items() if not 'w' in k}
+    if cell[1] >= len(grid)-1:
+        adjdict = {k:v for k,v in adjdict.items() if not 's' in k}
+    if cell[1] <= 0:
+        adjdict = {k:v for k,v in adjdict.items() if not 'n' in k}
+    
+    for neigh in adjdict.values():
+        if int(grid[neigh[0]][neigh[1]]) < 9:
+            print(grid[neigh[0]][neigh[1]])
+            cells.append(neigh)
+    return cells 
+
+def count(x, y, map):
+    counter = 0
+    children = get_adjacent_cells(map, (x,y))
+    children = [i for i in children if int(map[i[1]][i[0]]) < 9]
+    if len(children) > 0:
+        for c in children:
+            counter += count(x+c[0],y+c[1], map)
+    else:
+        counter += 1
+    return counter
+
+areas = []
+
+for mini in minis:
+    areas.append(count(mini[0][1],mini[0][0], map))
+
+
 
 print(f"result part 2: {sum([i[1]+1 for i in minis])}")
