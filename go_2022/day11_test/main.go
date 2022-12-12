e689b4fcca6da79d79520262cb4d53cf6adc8db5
Massimo <massimo.castell@gmail.com>
2022-12-12 20:25:48 +0100

    go 2022 day 11+12

--- go_2022/day11_test/main.go
+++ go_2022/day11_test/main.go
@@ -5,21 +5,13 @@ import "fmt"
 func monkeyop(m int, old uint64) uint64 {
 	switch m {
 	case 0:
-		return old * 7
+		return old * 19
 	case 1:
-		return old + 4
-	case 2:
-		return old + 2
-	case 3:
-		return old + 7
-	case 4:
-		return old * 17
-	case 5:
-		return old + 8
-	case 6:
 		return old + 6
-	case 7:
+	case 2:
 		return old * old
+	case 3:
+		return old + 3
 	}
 	return 0
 }
@@ -27,49 +19,25 @@ func monkeyop(m int, old uint64) uint64 {
 func monkeytest(m int, val uint64) bool {
 	switch m {
 	case 0:
-		if val%17 == 0 {
+		if val%23 == 0 {
 			return true
 		} else {
 			return false
 		}
 	case 1:
-		if val%3 == 0 {
-			return true
-		} else {
-			return false
-		}
-	case 2:
-		if val%5 == 0 {
-			return true
-		} else {
-			return false
-		}
-	case 3:
-		if val%7 == 0 {
-			return true
-		} else {
-			return false
-		}
-	case 4:
-		if val%11 == 0 {
-			return true
-		} else {
-			return false
-		}
-	case 5:
 		if val%19 == 0 {
 			return true
 		} else {
 			return false
 		}
-	case 6:
-		if val%2 == 0 {
+	case 2:
+		if val%13 == 0 {
 			return true
 		} else {
 			return false
 		}
-	case 7:
-		if val%13 == 0 {
+	case 3:
+		if val%17 == 0 {
 			return true
 		} else {
 			return false
@@ -81,30 +49,22 @@ func monkeytest(m int, val uint64) bool {
 func part2() {
 	counter := map[int]uint64{0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0}
 	monkeys := map[int][]uint64{
-		0: []uint64{54, 89, 94},
-		1: []uint64{66, 71},
-		2: []uint64{76, 55, 80, 55, 55, 96, 78},
-		3: []uint64{93, 69, 76, 66, 89, 54, 59, 94},
-		4: []uint64{80, 54, 58, 75, 99},
-		5: []uint64{69, 70, 85, 83},
-		6: []uint64{89},
-		7: []uint64{62, 80, 58, 57, 93, 56},
+		0: []uint64{79, 98},
+		1: []uint64{54, 65, 75, 74},
+		2: []uint64{79, 60, 97},
+		3: []uint64{74},
 	}
 
 	monkeytar := map[int][]int{
-		0: []int{5, 3},
-		1: []int{0, 3},
-		2: []int{7, 4},
-		3: []int{5, 2},
-		4: []int{1, 6},
-		5: []int{2, 7},
-		6: []int{0, 1},
-		7: []int{6, 4},
+		0: []int{2, 3},
+		1: []int{2, 0},
+		2: []int{1, 3},
+		3: []int{0, 1},
 	}
 
 	for round := 1; round <= 10000; round++ {
 		fmt.Println(round)
-		for x := 0; x <= 7; x++ {
+		for x := 0; x <= 3; x++ {
 			items := len(monkeys[x])
 			for i := 0; i < len(monkeys[x]); i++ {
 				val := monkeys[x][i]
