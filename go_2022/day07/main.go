bb13f6317ca7f9e44b183b83356bce097fb7cab1
Massimo <massimo.castell@gmail.com>
2022-12-27 23:32:28 +0100

    go 2022 day 7

--- go_2022/day07/main.go
+++ go_2022/day07/main.go
@@ -4,8 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
+
+type Directory struct {
+	Parent string
+	Files  map[string]int
+	Childs []string
+}
 
 func part1() {
 
@@ -17,29 +24,58 @@ func part1() {
 
 	fileScanner := bufio.NewScanner(f)
 	fileScanner.Split(bufio.ScanLines)
-
-	var path = map[string]string{}
+	parent := ""
+	actual := ""
+	m := make(map[string]Directory)
 
 	for fileScanner.Scan() {
 		raw := strings.Fields(fileScanner.Text())
-		fmt.Println(raw)
-		switch raw[0] {
+		fmt.Println(raw, parent)
+		sw := ""
+		if _, err := strconv.Atoi(raw[0]); err == nil {
+			sw = "no"
+		} else {
+			sw = raw[0]
+		}
+
+		switch sw {
 		case "$":
-			switch raw[0] {
-
+			switch raw[1] {
+			case "cd":
+				parent = actual
+				actual = raw[2]
+				_, ok := m[actual]
+				if !ok {
+					m[actual] = Directory{Parent: parent}
+				}
+			case "ls":
+				continue
 			}
+		case "dir":
+			m[raw[1]] = Directory{Parent: actual}
+			ad := m[actual]
+			ad.Childs = append(ad.Childs, raw[1])
+
+		case "no":
+			v, _ := strconv.Atoi(raw[0])
+			ad := m[actual]
+			af := ad.Files
+			// https://stackoverflow.com/questions/18042439/go-append-to-slice-in-struct
+			fmt.Println(raw, parent)
+			af[raw[1]] = v
+			fmt.Println(raw, parent)
 
 		}
-		for _, x := range raw {
-			fmt.Println(x)
-			x = string(x)
-			switch x {
-			case "$":
-				fmt.Println("command")
-			case "dir":
-				fmt.Println("dir")
-			}
-		}
+		// for _, x := range raw {
+		// 	fmt.Println(x)
+		// 	x = string(x)
+		// 	switch x {
+		// 	case "$":
+		// 		fmt.Println("command")
+		// 	case "dir":
+		// 		fmt.Println("dir")
+		// 	}
+		// }
 	}
 	// quant, _ := strconv.Atoi(x[1])
 	// from, _ := strconv.Atoi(x[3])
