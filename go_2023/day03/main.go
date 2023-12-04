f5f1d4112ba7961270436cd4ea40acc186ed3701
Massimo <massimo.castell@gmail.com>
2023-12-04 01:18:39 +0100

    2023 day 3 correction

--- go_2023/day03/main.go
+++ go_2023/day03/main.go
@@ -7,29 +7,27 @@ import (
 	"slices"
 )
 
-func valcheck(data []byte, idx int, lenght int) bool {
-	m := map[string]func(int, int) int{
-		"n":  func(idx int, lenght int) int { return idx - lenght },
-		"ne": func(idx int, lenght int) int { return idx - lenght + 1 },
-		"e":  func(idx int, lenght int) int { return idx + 1 },
-		"se": func(idx int, lenght int) int { return idx + lenght + 1 },
-		"s":  func(idx int, lenght int) int { return idx + lenght },
-		"sw": func(idx int, lenght int) int { return idx + lenght - 1 },
-		"w":  func(idx int, lenght int) int { return idx - 1 },
-		"nw": func(idx int, lenght int) int { return idx - lenght - 1 },
+func valcheck(data [][]byte, row int, col int, hight int, width int) bool {
+	m := map[string]func([][]byte, int, int) byte{
+		"n":  func(data [][]byte, row int, col int) byte { return data[row-1][col] },
+		"ne": func(data [][]byte, row int, col int) byte { return data[row-1][col+1] },
+		"e":  func(data [][]byte, row int, col int) byte { return data[row][col+1] },
+		"se": func(data [][]byte, row int, col int) byte { return data[row+1][col+1] },
+		"s":  func(data [][]byte, row int, col int) byte { return data[row+1][col] },
+		"sw": func(data [][]byte, row int, col int) byte { return data[row+1][col-1] },
+		"w":  func(data [][]byte, row int, col int) byte { return data[row][col-1] },
+		"nw": func(data [][]byte, row int, col int) byte { return data[row-1][col-1] },
 	}
 
 	ways := []string{"n", "ne", "e", "se", "s", "sw", "w", "nw"}
 
-	if (data[m["e"](idx, lenght)] >= 48) && (data[m["e"](idx, lenght)] <= 57) {
-		return valcheck(data, idx+1, lenght)
+	if ((m["e"](data, row, col)) >= 48) && (m["e"](data, row, col) <= 57) {
+		return valcheck(data, row, col+1, hight, width)
 	} else {
 		for _, way := range ways {
-			if !(m[way](idx, lenght) < 0) || (m[way](idx, lenght) > lenght) {
-				//fmt.Println(data[m[way](idx, lenght)])
-				//if !(data[m[way](idx, lenght)] == 46 || data[m[way](idx, lenght)] == 10 || data[m["e"](idx, lenght)] >= 48 || data[m["e"](idx, lenght)] >= 57) {
+			if !(row < 0 || row > hight || col < 0 || col > width) {
 				numbers := []byte{46, 10, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57}
-				if !(slices.Contains(numbers, data[m[way](idx, lenght)])) {
+				if !(slices.Contains(numbers, m[way](data, row, col))) {
 					return true
 				}
 			}
@@ -37,25 +35,31 @@ func valcheck(data []byte, idx int, lenght int) bool {
 		}
 		return false
 	}
-	return false
 }
 
 func part1() {
 	data, _ := os.ReadFile("sample.txt")
 	grid := bytes.Fields(data)
-	lenght := len(grid) + 1
-	fmt.Println(grid)
-	res := make([]byte, lenght)
-	for idx, _ := range data {
-		if !(valcheck(data, idx, lenght)) {
-			//res[idx] = byte(".")
-			copy(res[idx:], ".")
-		} else {
-			res[idx] = data[idx]
-		}
+	hight := len(grid)
+	width := len(grid[0])
+	res := make([][]byte, hight)
+	for i := range res {
+		res[i] = make([]byte, width)
 	}
 
-	// fmt.Println("Part 1:", result)
+	for row, _ := range grid {
+		for col, _ := range grid[row] {
+			if !(valcheck(grid, row, col, hight, width)) {
+				//res[idx] = byte(".")
+				res[row][col] = 46
+			} else {
+				res[row][col] = grid[row][col]
+			}
+		}
+		fmt.Println(grid)
+		fmt.Println(res)
+	}
+	fmt.Println("Part 1:", grid)
 }
 
 func main() {
